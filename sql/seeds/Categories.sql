b294006fb524117baac1409cc59ccc85de32d732
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-26 20:00:28 +0545

    feat: sql file add

---
+++ sql/seeds/Categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('1', 'Beverages', 'Soft drinks, coffees, teas, beers, and ales');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('2', 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('3', 'Confections', 'Desserts, candies, and sweet breads');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('4', 'Dairy Products', 'Cheeses');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('5', 'Grains/Cereals', 'Breads, crackers, pasta, and cereal');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('6', 'Meat/Poultry', 'Prepared meats');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('7', 'Produce', 'Dried fruit and bean curd');
+INSERT INTO tableName (categoryid, categoryname, description) VALUES ('8', 'Seafood', 'Seaweed and fish');
